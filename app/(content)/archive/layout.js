579af11512eb96a407bf45e407be213376a0ca76
DumitriuAndrei <72802470+DumitriuAndrei@users.noreply.github.com>
2024-05-24 19:07:58 +0300

    caching-app

--- app/(content)/archive/layout.js
+++
@@ -1,9 +0,0 @@
-export default function ArchiveLayout({archive, latest}) {
-    return (
-        <div>
-            <h1>News Archive</h1>
-            <section id="archive-filter">{archive}</section>
-            <section id="latest-filter">{latest}</section>
-        </div>
-    )
-}
--- app/(content)/archive/loading.js
+++
@@ -1,3 +0,0 @@
-export default function LoadingLatestNews() {
-    return <p>Loading...</p>
-}
